aba40950dc002525ba2c1709fe5638aa5bb9fb62
yichengniu <giggsnbc@outlook.com>
2020-07-30 20:50:25 -0400

    segment tree implement

--- java practice/src/JavaDataStructure/segmentTree.java
+++ java practice/src/JavaDataStructure/segmentTree.java
@@ -1,14 +1,14 @@
 package JavaDataStructure;
 
 public class segmentTree {
-    public static SegmentTreeNode build(int start, int end, int[] array) {
+    public static SegmentTreeNode buildTree(int start, int end, int[] array) {
         if (start == end) {
             return new SegmentTreeNode(start, end, array[start]);
 
         }
         int mid = (end + start) / 2;
-        SegmentTreeNode left = build(start, mid, array);
-        SegmentTreeNode right = build(mid + 1, end, array);
+        SegmentTreeNode left = buildTree(start, mid, array);
+        SegmentTreeNode right = buildTree(mid + 1, end, array);
 
         return new SegmentTreeNode(start, end, left.sum + right.sum, left, right);
 
@@ -16,7 +16,7 @@ public class segmentTree {
 
     public static void main(String[] args) {
         int[] array = {1,2,3,4,55,64,2,2};
-        SegmentTreeNode a = build(0, array.length - 1, array);
+        SegmentTreeNode a = buildTree(0, array.length - 1, array);
 
     }
 }
