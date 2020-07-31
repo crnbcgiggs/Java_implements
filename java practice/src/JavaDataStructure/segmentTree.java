8d217ef6b262f1be116ee8f9f60affc37b3c2f1b
yichengniu <giggsnbc@outlook.com>
2020-07-31 01:02:42 -0400

    implements construction, update and query sum for a segment tree

--- java practice/src/JavaDataStructure/segmentTree.java
+++ java practice/src/JavaDataStructure/segmentTree.java
@@ -27,12 +27,27 @@ public class segmentTree {
         }
         root.sum = root.left.sum + root.right.sum;
     }
+    public static int querySum(SegmentTreeNode root, int start, int end) {
+        if (start == root.start && end == root.end) {
+            return root.sum;
+        }
+        int mid = root.start + (root.end - root.start) / 2;
+        if (end <= mid) {
+            return querySum(root.left, start, end);
+        }
+        if (start >= mid + 1) {
+            return querySum(root.right, start, end);
+        }
+        else {
+            return querySum(root.left, start, mid) + querySum(root.right, mid + 1, end);
+        }
+    }
 
     public static void main(String[] args) {
         int[] array = {1,2,3,4,55,64,2,2};
         SegmentTreeNode a = buildTree(0, array.length - 1, array);
         updateTree(a, 3, 52);
-
+        System.out.println(querySum(a, 3, 5));
 
     }
 }
