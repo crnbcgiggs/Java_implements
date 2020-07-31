06370887658dc28efaeac6c3701456d75fc6a8b2
yichengniu <giggsnbc@outlook.com>
2020-07-30 20:19:55 -0400

    segment tree implement

--- java practice/src/JavaDataStructure/SegmentTreeNode.java
+++ java practice/src/JavaDataStructure/SegmentTreeNode.java
@@ -1,4 +1,9 @@
 package JavaDataStructure;
 
 public class SegmentTreeNode {
+    public int start;
+    public int end;
+    public int sum;
+    SegmentTreeNode left;
+    SegmentTreeNode right;
 }
